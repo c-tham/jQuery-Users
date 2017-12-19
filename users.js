4b42f7d8569367c491553d192791f81fdfa3f8df
Chia T <tham_oh@hotmail.com>
2017-12-18 19:16:25 -0600

    correction on the table output (remove adduser)

--- users.js
+++ users.js
@@ -4,7 +4,12 @@ $(document).ready(function() {
         // $('tbody').append('<tr><td>test1</td><td>test2</td><td>test3</td><td>test4</td></tr>');
         console.log($(this).serialize());
         // *** test map
-        $('tbody').append('<tr><td>'+$('input').map(function() { return $(this).val() }).get() . join('</td><td>')+'</td></tr>');
+        // $('tbody').append('<tr><td>'+$('input').map(function() { return $(this).val() }).get() . join('</td><td>')+'</td></tr>');
+        $('tbody').append('<tr><td>' 
+            + $('input[name="firstname"]').val() + '</td><td>'
+            + $('input[name="lastname"]').val() + '</td><td>'
+            + $('input[name="email"]').val() + '</td><td>'
+            + $('input[name="phonenumber"]').val() + '</td></tr>')
         return false;
     });
 
